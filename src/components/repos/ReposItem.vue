fd4278a5429d035057a94918d4b67a57296cf830
andyiac <andyiaczhang@gmail.com>
2016-11-21 16:10:51 +0800

    modify project item

--- src/components/repos/ReposItem.vue
+++ src/components/repos/ReposItem.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="repo-wrap">
-        <div class="avatar"> <a href=""><img src="" alt=""></a></div>
-        <div class="item"><a :href=repo.html_url  target="_blank"> {{ this.linkName }}</a></div>
+        <div class="item">
+            <a  class="content" :href=repo.html_url  target="_blank"> 
+                <div class="avatar"> avatar </div>
+                <div class="name"> {{ repo.name }} </div>
+                <div class="desc"> {{ repo.description }}</div>
+            </a>
+        </div>
+        
     </div>
 </template>
 
@@ -9,25 +15,43 @@
 
 #repo-wrap
     width 100%
-    height 60px 
+    height 100px 
     .item
-        line-height 60px
         position relative
         padding-left 20px
     .item a:before , .item a:after
-        border 8px solid transparent
-        border-left 8px solid #fff
+        border 10px solid transparent
+        border-left 10px solid #fff
         width 0
         height 0
         position absolute
-        top 35%
+        top 70%
         right 20px
         content ' '
     .item a:before
         border-left-color #9e9ea6
         right 18px
+    .item .content
+        .avatar
+            line-height 100px
+            float left
+            width 80px
+            background grey
+        .name
+            text-align left
+            float left
+            width 120px
+            height 100%
+            margin-left 20px
+        .desc 
+            margin-left 20px
+            float left
+            max-width 600px
+
     a 
         text-decoration none
+        display block
+
 
     
 </style>
