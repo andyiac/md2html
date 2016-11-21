9dd91421b9b3963cf33f02a8efd6ba03559ccfeb
andyiac <andyiaczhang@gmail.com>
2016-11-21 15:42:39 +0800

    add project arrow

--- src/components/repos/ReposItem.vue
+++ src/components/repos/ReposItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="repo-wrap">
-        <div class="item"><a :href=repo.html_url  target="_blank"> {{ this.linkName }}</a></div>
         <div class="avatar"> <a href=""><img src="" alt=""></a></div>
+        <div class="item"><a :href=repo.html_url  target="_blank"> {{ this.linkName }}</a></div>
     </div>
 </template>
 
@@ -12,7 +12,23 @@
     height 60px 
     .item
         line-height 60px
+        position relative
         padding-left 20px
+    .item a:before , .item a:after
+        border 8px solid transparent
+        border-left 8px solid #fff
+        width 0
+        height 0
+        position absolute
+        top 35%
+        right 20px
+        content ' '
+    .item a:before
+        border-left-color #9e9ea6
+        right 18px
+    a 
+        text-decoration none
+
     
 </style>
 
