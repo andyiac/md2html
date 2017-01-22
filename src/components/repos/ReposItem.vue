785a49cfdc82a2d8edca4484cc893810753d2f27
andyiac <andyiaczhang@gmail.com>
2017-01-22 13:27:48 +0800

    modify style

--- src/components/repos/ReposItem.vue
+++ src/components/repos/ReposItem.vue
@@ -1,11 +1,7 @@
 <template>
     <div id="repo-wrap">
-        <div class="item">
-            <div class="content"> 
-                <router-link class="name" :to="current_url">{{ repo.title }}</router-link>
-                <div class="time"> {{repo.time }}</div>
-            </div>
-        </div>
+        <router-link class="name" :to="current_url">{{ repo.title }}</router-link>
+        <div class="time"> {{repo.time }}</div>
     </div>
 </template>
 
@@ -24,40 +20,28 @@ export default{
 
 #repo-wrap
     width 100%
-    border-bottom 1px solid #f7f7f7
-    .item
-        position relative
-    .item a:before , .item a:after
-        border 8px solid transparent
-        border-left 8px solid #fff
-        width 0
-        height 0
-        position absolute
-        top 20px 
-        right 20px
-    .item a:before
-        border-left-color #9e9ea6
-        right 18px
-    .item .content
+    border-bottom 1px solid #e6e6e6
+    clear both 
+    .name
         padding 30px 0
-        .name
-            font-size 17px
-            text-align left
-            float left
-            padding-bottom 1px
-            text-overflow ellipsis
-            overflow hidden
-            height 100%
-            width 70%
-            color #555459
-        .name:hover
-            color #42b983
-        .time
-            height 100%
-            font-size 14px
-            float right 
-            color rgba(0,0,0,0.3)
-            font-style italic
+        font-size 17px
+        text-align left
+        float left
+        text-overflow ellipsis
+        overflow hidden
+        height 100%
+        width 70%
+        color #555459
+    .name:hover
+        color #42b983
+    .time
+        padding 30px 0
+        height 100%
+        font-size 14px
+        width 30%
+        text-align right
+        float right 
+        color rgba(0,0,0,0.3)
     a 
         text-decoration none
         display block
