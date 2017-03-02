355a7b6b1f1c2c592d83f6f08e8eb2b3f36f03ab
andyiac <andyiaczhang@gmail.com>
2017-03-02 17:21:56 +0800

    basic done

--- src/components/Md2Html.vue
+++ src/components/Md2Html.vue
@@ -6,7 +6,9 @@
             <button @click="download()">Download</button>
         </div>
         <div class="right-area">
-            <div id="download-content" class="markdown markdown-body github" v-bind:style="cssSource" v-html="htmlSource"></div>
+            <div id="download-content" >
+                <div class="markdown markdown-body github" v-html="htmlSource"></div>
+            </div>
         </div>
         <iframe frameborder="0"> </iframe>
     </div>
@@ -19,7 +21,7 @@ marked.setOptions({
     renderer: new marked.Renderer(),
     gfm: true,
     tables: true,
-    breaks: false,
+    breaks: true,
     pedantic: false,
     sanitize: false,
     smartLists: true,
@@ -40,21 +42,25 @@ export default{
             this.htmlSource = marked(this.markdownSource)
             if(this.cssSource){
                 var style = '<style>' + this.cssSource +'</style>'
-                this.htmlSource += style
+                this.htmlSource = style + this.htmlSource
             }
         },
         download(){
+            var elHtml = document.getElementById("download-content").innerHTML;
             var a = document.body.appendChild(
                 document.createElement("a")
             );
             a.download = "export.html";
-            // a.href = "data:text/html," + document.getElementById("download-content").innerHTML; // Grab the HTML
-            a.href = "data:text/html," + document.getElementById("download-content").src; // Grab the HTML
-            a.click(); // Trigger a click on the element
+            // Grab the HTML
+            a.href = "data:text/html;charset=utf-8," + encodeURIComponent(elHtml);
+            // Trigger a click on the element
+            a.click();
         }
     },
     watch:{
-        markdownSource: 'md2html'
+        markdownSource: 'md2html',
+        cssSource: 'md2html'
+
     }
 }
     
